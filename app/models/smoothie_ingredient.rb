96ba2615c5c4838475b10a022bab39d26de527ce
github username <crishanks12@gmail.com>
2019-02-25 09:32:44 -0800

    rails app setup

---
+++ app/models/smoothie_ingredient.rb
@@ -0,0 +1,2 @@
+class SmoothieIngredient < ApplicationRecord
+end
---
+++ db/migrate/20190225172536_create_smoothie_ingredients.rb
@@ -0,0 +1,9 @@
+class CreateSmoothieIngredients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :smoothie_ingredients do |t|
+      t.integer :smoothie_id
+      t.integer :ingredient_id
+      t.timestamps
+    end
+  end
+end
